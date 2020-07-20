c10f7796a04de1dfb62a29c0487de786d9455a09
macondo <jl.durand95@gmail.com>
2020-07-19 20:14:51 -0400

    gitUpdate

--- lib/database/procedures/spMasaTipoAddEdit.sql
+++ lib/database/procedures/spMasaTipoAddEdit.sql
@@ -1,19 +1,19 @@
 USE `parron`;
 DROP PROCEDURE IF EXISTS `addOrEditMasaTipo`;
 DELIMITER $$
-CREATE PROCEDURE `addOrEditMasaSabor` (
+CREATE PROCEDURE `addOrEditMasaTipo` (
     IN _id INT(11),
     IN _nombre VARCHAR(200))
 BEGIN
     IF _id = 0 THEN
-        INSERT	INTO masaSabor (
+        INSERT	INTO masaTipo (
             nombre)
         VALUES(
             _nombre
         );
         SET _id = LAST_INSERT_ID();
     ELSE
-        UPDATE masaSabor
+        UPDATE masaTipo
         SET 
             nombre = _nombre
         WHERE id = _id;
