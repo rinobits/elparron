4124a97d0425897852598f7b6c74449a7108a395
macondo <jl.durand95@gmail.com>
2020-07-26 23:29:00 -0400

    26jul_2020

---
+++ docs/nodeDocs/service/SemanalService.js
@@ -0,0 +1,78 @@
+'use strict';
+
+
+/**
+ * Crear programacion semanal
+ * Se requieren privilegios.
+ *
+ * body JsonData  (optional)
+ * fecha String fecha de solicitud \"DD-MM-YYYY\" (optional)
+ * sucursal_id Integer id de la sucursal (optional)
+ * returns createTrue
+ **/
+exports.createSemanal = function(body,fecha,sucursal_id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "CREATED" : true
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Editar información
+ * Se requieren privilegios.
+ *
+ * body JsonData  (optional)
+ * fecha String fecha de solicitud \"DD-MM-YYYY\" (optional)
+ * sucursal_id Integer id de la sucursal (optional)
+ * returns modifyTrue
+ **/
+exports.editSemanal = function(body,fecha,sucursal_id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "MODIFY DATA" : true
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+/**
+ * Buscar dia por fecha e id de la sucursal.
+ * Se requieren privilegos
+ *
+ * fecha String fecha de solicitud \"DD-MM-YYYY\" (optional)
+ * sucursal_id Integer id de la sucursal (optional)
+ * returns getJsonData
+ **/
+exports.getSemanalById = function(fecha,sucursal_id) {
+  return new Promise(function(resolve, reject) {
+    var examples = {};
+    examples['application/json'] = {
+  "sucursal_id" : 1,
+  "createdAt" : "2020-07-25 17:14:27",
+  "id" : 5,
+  "dia" : 3,
+  "detalle" : [ "", "" ],
+  "updatedAt" : "2020-07-25 17:14:27"
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
+  });
+}
+
